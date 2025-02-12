cbc2bcb1b3270d90a197bdb484a37afed52b08c2
Xander <xanderdshotz@gmail.com>
2025-02-12 10:22:58 -0500

    Added current limits for motors on the elevator

--- src/main/java/frc/robot/CTREConfigs.java
+++ src/main/java/frc/robot/CTREConfigs.java
@@ -20,20 +20,34 @@ public class CTREConfigs {
         ELEVATOR_CONFIG.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
         ELEVATOR_CONFIG.SoftwareLimitSwitch.ReverseSoftLimitThreshold = 0;
         ELEVATOR_CONFIG.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+        ELEVATOR_CONFIG.CurrentLimits.StatorCurrentLimitEnable = true;
+        ELEVATOR_CONFIG.CurrentLimits.StatorCurrentLimit = 50;
 
         ELEVATOR_CONFIG.CurrentLimits.SupplyCurrentLimitEnable = true;
-        ELEVATOR_CONFIG.CurrentLimits.SupplyCurrentLimit = 40;
+        ELEVATOR_CONFIG.CurrentLimits.SupplyCurrentLimit = 50;
 
         // Ball Intake Config
         BALL_INTAKE_CONFIG.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+        BALL_INTAKE_CONFIG.CurrentLimits.StatorCurrentLimitEnable = true;
+        BALL_INTAKE_CONFIG.CurrentLimits.StatorCurrentLimit = 50;
+        BALL_INTAKE_CONFIG.CurrentLimits.SupplyCurrentLimitEnable = true;
+        BALL_INTAKE_CONFIG.CurrentLimits.SupplyCurrentLimit = 50;
         
         // Pipe Intake Config
         PIPE_INTAKE_CONFIG.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+        PIPE_INTAKE_CONFIG.CurrentLimits.StatorCurrentLimitEnable = true;
+        PIPE_INTAKE_CONFIG.CurrentLimits.StatorCurrentLimit = 50;
+        PIPE_INTAKE_CONFIG.CurrentLimits.SupplyCurrentLimitEnable = true;
+        PIPE_INTAKE_CONFIG.CurrentLimits.SupplyCurrentLimit = 50;
 
         // Intake Pivot Config
         WRIST_CONFIG.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
         WRIST_CONFIG.SoftwareLimitSwitch.ForwardSoftLimitThreshold = 100;
         WRIST_CONFIG.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+        WRIST_CONFIG.CurrentLimits.StatorCurrentLimitEnable = true;
+        WRIST_CONFIG.CurrentLimits.StatorCurrentLimit = 50;
+        WRIST_CONFIG.CurrentLimits.SupplyCurrentLimitEnable = true;
+        WRIST_CONFIG.CurrentLimits.SupplyCurrentLimit = 100;
     }
 
     /**
